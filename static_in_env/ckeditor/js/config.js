fa0eb61244f7a6d8e8dfbbc7305fc7a28e7235a8
James Reeves <reeves_james@outlook.com>
2019-11-19 13:58:19 -0800

    added new file config.js to ckeditor static files. includes a list of supported languages for codesnippet. Then added the script into scripts.html

---
+++ static_in_env/ckeditor/js/config.js
@@ -0,0 +1,12 @@
+CKEDITOR.editorConfig = function( config ) {
+	config.codeSnippet_languages = {
+        cs: 'C#',
+        css: 'CSS',
+        html: 'HTML',
+        javascript: 'JavaScript',
+        json: 'JSON',
+        python: 'Python',
+        sql: 'SQL',
+        vbnet: 'vba'
+    };
+};
