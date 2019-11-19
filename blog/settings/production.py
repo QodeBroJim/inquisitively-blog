6122666d6713c8bf9f83dc57e5d151be0fd02232
James Reeves <reeves_james@outlook.com>
2019-11-19 13:42:42 -0800

    adding vba support in production.py for ckeditor codesnippet

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -190,6 +190,16 @@ CKEDITOR_CONFIGS = {
         'extraPlugins': ','.join([
             'codesnippet', 'widget', 'lineutils'
         ]),
+        'codesnippet_languages' = {
+            'cs':"C#",
+            'css':"CSS",
+            'html':"HTML",
+            'javascript':"JavaScript",
+            'json':"JSON",
+            'python':"Python",
+            'sql':"SQL",
+            'vbnet': "VBA"
+        }
     }
 }
 
