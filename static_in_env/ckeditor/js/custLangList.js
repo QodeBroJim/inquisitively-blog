131aa07b6aaee5a96cc12b403647d6730c7234b0
James Reeves <reeves_james@outlook.com>
2019-11-19 19:13:07 -0800

    Changing js files to hopefully update the list of programming languages in codesnippet

---
+++ static_in_env/ckeditor/js/custLangList.js
@@ -0,0 +1,22 @@
+CKEDITOR.editorConfig = function( config ) {
+	config.codeSnippet_languages = {
+		bash:"Bash",
+		cpp:"C++",
+		cs:"C#",
+		css:"CSS",
+		dart:"Dart",
+		django:"Django",
+		go:"Go",
+		html:"HTML",
+		javascript:"JavaScript",
+		json:"JSON",
+		kotlin:"Kotlin",
+		matlab:"Matlab",
+		python:"Python",
+		sql:"SQL",
+		swift:"Swift",
+		typescript:"TypeScript",
+		vb:"Visual Basic",
+		xml:"XML"
+	};
+};
