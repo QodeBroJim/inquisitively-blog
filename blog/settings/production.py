c0cb22f2876c0886b03d0aa9c19e335c6c0c10b7
James Reeves <reeves_james@outlook.com>
2019-11-19 18:33:48 -0800

    settings for codesnippet languages

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -190,7 +190,27 @@ CKEDITOR_CONFIGS = {
         'extraPlugins': ','.join([
             'codesnippet', 'widget', 'lineutils'
         ]),
-    }
+    },
+    config.codeSnippet_languages = {
+        bash:"Bash",
+        cpp:"C++",
+        cs:"C#",
+        css:"CSS",
+        dart:"Dart",
+        django:"Django",
+        go:"Go",
+        html:"HTML",
+        javascript:"JavaScript",
+        json:"JSON",
+        kotlin:"Kotlin",
+        matlab:"Matlab",
+        python:"Python",
+        sql:"SQL",
+        swift:"Swift",
+        typescript:"TypeScript",
+        vb:"Visual Basic",
+        xml:"XML"
+        }; 
 }
 
 # Crispy Forms
